bd993b1d2f834a8260670e163c4f56286e24aa30
eldom11 <2210631170065@student.unsika.ac.id>
2024-10-21 18:54:08 +0700

    membuat variable untuk menampung setiap data yang diinginkan

--- pokedex/backend/generateJsonDB.js
+++ pokedex/backend/generateJsonDB.js
@@ -8,7 +8,20 @@ async function generateJsonDB() {
   try{
     const pokemonApiURL = "https://pokeapi.co/api/v2/pokemon/";
     const getPokemon = await fetch(pokemonApiURL).then((res) => res.json());
+    const pokemonArray = [];
 
+    for (let i = 0; i < getPokemon.results.length; i++) {
+      const pokemonInfo = getPokemon.results[i];
+      const pokemonDetail = await fetch(pokemonInfo.url).then((res) => res.json());
+      const pokemonSpecies = await fetch(pokemonDetail.species.url).then((res) => res.json());
+      const pokemonEvolution = await fetch(pokemonSpecies.evolution_chain.url).then((res) => res.json());
+      const pokemonEvolutionChain = [pokemonEvolution.chain.species.name];
+      let pokemonEvolveTo = pokemonEvolution.chain.evolves_to[0];
+      while (pokemonEvolveTo) {
+        pokemonEvolutionChain.push(pokemonEvolveTo.species.name);
+        pokemonEvolveTo = pokemonEvolveTo.evolves_to[0];
+      }
+    }
   } catch(error) {
     console.error(error)
   }
